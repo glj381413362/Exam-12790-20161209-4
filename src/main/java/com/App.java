e44129fb979cbc53eac843e81024a9ff4a4279e1
GongLiangJun <381413362@qq.com>
2016-12-09 22:23:27 +0800

    修改最后结果

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -8,23 +9,78 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-/**
- * Hello world!
- * 
- */
+
 public class App {
+	  
+	    /**     
+	     * @discription 
+	     * @author 龚梁钧       
+	     * @created Dec 9, 2016 10:22:28 PM     
+	     * @param args     
+	     */
 	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<Integer>();
-		StringBuffer sBuffer = new StringBuffer(); 
-		sBuffer.append("随机生成50 个小于100 的数,分别为:");
-		for (int i = 0; i < 50; i++) {
-			Random r = new Random();
-			int j = r.nextInt(100)+1;
-			list.add(j);
-			sBuffer.append(j);
-			sBuffer.append(",");
+		List<Integer> list = getRandList();
+		Set<Map.Entry<Integer, HashSet<Integer>>> noSortMapResulst = noSortMap(list);
+		Set<Map.Entry<Integer, TreeSet<Integer>>> sortMapResulst = sortMap(list);
+		System.out.println("Map 中的数据为:"+noSortMapResulst);
+		System.out.println("排序后的Map为: "+sortMapResulst);
+	}
+	
+	    
+	    /**     
+	     * @discription 获得50个随机数
+	     * @author 龚梁钧       
+	     * @created Dec 9, 2016 10:17:29 PM     
+	     * @return  List <Integer>   
+	     */
+	public static List<Integer> getRandList() {
+		  List<Integer> list = new ArrayList<Integer>();
+			StringBuffer sBuffer = new StringBuffer(); 
+			sBuffer.append("随机生成50 个小于100 的数分别为:");
+			for (int i = 0; i < 50; i++) {
+				Random r = new Random();
+				int j = r.nextInt(100)+1;
+				list.add(j);
+				sBuffer.append(j);
+				if (list.size()!=50)
+					sBuffer.append(",");
+			}
+			System.out.println(sBuffer);  //输出得到的50个随机数
+			return list;
+	}
+	  
+	    /**     
+	     * @discription 获得未排序之前的结果
+	     * @author 龚梁钧       
+	     * @created Dec 9, 2016 10:14:53 PM     
+	     * @param list
+	     * @return     
+	     */
+	public static Set<Map.Entry<Integer, HashSet<Integer>>> noSortMap(List<Integer> list) {
+		TreeMap<Integer, HashSet<Integer>> map = new TreeMap<Integer, HashSet<Integer>>();
+		for (int i : list) {
+			int j = i / 10;
+			if (map.keySet().contains(j)) {
+				HashSet<Integer> set = map.get(j);
+				set.add(i);
+				map.put(j, set);
+			} else {
+				HashSet<Integer> set = new HashSet<Integer>();
+				set.add(i);
+				map.put(j, set);
+			}
 		}
-		System.out.println(sBuffer);
+		return map.entrySet();
+	}
+	
+	    /**     
+	     * @discription 得到排序后的結果
+	     * @author 龚梁钧       
+	     * @created Dec 9, 2016 10:09:17 PM     
+	     * @param list
+	     * @return     
+	     */
+	public static Set<Map.Entry<Integer, TreeSet<Integer>>> sortMap(List<Integer> list){
 		TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<Integer, TreeSet<Integer>>();
 		for (int i : list) {
 			int j = i / 10;
@@ -38,8 +94,7 @@ public class App {
 				map.put(j, set);
 			}
 		}
-		System.out.println(map);
-		Set<Map.Entry<Integer, TreeSet<Integer>>> e = map.entrySet();
-		System.out.println(e);
+		return map.entrySet();
 	}
+	
 }
